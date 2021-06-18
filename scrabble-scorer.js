200ab21c0e95cc75e3dd261986a573c5f13ae998
nihakon <84293481+Nihakon06@users.noreply.github.com>
2021-06-18 21:54:10 +0000

    Wrote Initial Code

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -26,32 +26,99 @@ function oldScrabbleScorer(word) {
  
 	  }
 	}
-	return letterPoints;
+  return letterPoints;
  }
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some Scrabble!");
+   word = input.question("Enter a word to score: ");
+   return word;
+   };
+
+let simpleScore = function(word) {
+  return word.length;
+
 };
 
-let simpleScore;
+let vowelBonusScore = function(word) {
+  const vowels = ["a","e","i","o","u"]; 
+  word = word.toUpperCase(); 
+  let vowelScore = 0; 
+  for(let i = 0; i<word.length; i++) {
+    if(vowels.includes(word[i])===true) {
+      vowelScore += 3;
+    }
+    else{
+      vowelScore += 1;
+    }
+  }
+  return vowelScore;
+};
 
-let vowelBonusScore;
+let scrabbleScore = function(word) {
+  let scrabbleScore = 0;
+  for(let i = 0; i<word.length; i++) {
+    scrabbleScore +=newPointStructure[word[i]];
+    }
+  return scrabbleScore;
+};
 
-let scrabbleScore;
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [
+  {
+    name: 'Simple Score',
+    description: 'Each letter is worth 1 point.', 
+    scorerFunction: simpleScore
+  },
+  {
+    name: 'Simple Score',
+    description: 'Each letter is worth 1 point.', 
+    scorerFunction: vowelBonusScore
+  },
+  {
+    name: 'Simple Score',
+    description: 'Each letter is worth 1 point.', 
+    scorerFunction: scrabbleScore
+  }
+];
 
-function scorerPrompt() {}
 
-function transform() {};
+function scorerPrompt(word) {
+  selectedInput = input.question(`Which scoring algorithm would you like to use? \n
+  0 - ${scoringAlgorithms[0].name}: One point per character \n
+  1 - ${scoringAlgorithms[1].name}: Vowels are worth 3 points \n
+  2 - ${scoringAlgorithms[2].name}: Uses scrabble point system \n
+  Enter 0, 1, or 2: `);
+  if (selectedInput === '0') {
+    return (`Score for ${word}: ${scoringAlgorithms[0].scorerFunction(word)}`);
+  } else if (selectedInput === '1') {
+    return (`Score for ${word}: ${scoringAlgorithms[1].scorerFunction(word)}`);
+  } else if (selectedInput === '2') {
+    return (`Score for ${word}: ${scoringAlgorithms[2].scorerFunction(word)}`);
+  } else {
+    console.log('Invalid Input');
+  }
+}
 
-let newPointStructure;
+function transform(object) {
+  let newPointObject = {};
+  for (item in object){
+    for (i = 0; i< object[item].length; i++){
+      let key = object[item][i];
+      newPointObject[`${key}`]= Number(item);
+    }
+  }
+  return newPointObject;
+};
+
+let newPointStructure = transform(oldPointStructure);
 
 function runProgram() {
-   initialPrompt();
+   word = initialPrompt();
+   scorerPrompt(word);
    
 }
 
